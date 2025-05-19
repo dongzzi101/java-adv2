4cc5d93341d22047b97ed7db2c996f5f66bd0b0b
dongwook <dongzzi101@gmail.com>
2025-05-19 13:04:06 +0900

    [문자 다루기1 - 시작] :
    스트림의 모든 데이터는 byte 단위 사용 → byte가 아닌 문자를 스트림에 직접 전달 불가

---
+++ src/io/text/ReaderWriterMainV1.java
@@ -0,0 +1,41 @@
+package io.text;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+import static io.text.TextConst.*;
+import static java.nio.charset.StandardCharsets.*;
+
+public class ReaderWriterMainV1 {
+
+    public static void main(String[] args) throws IOException {
+        String writeString = "ABC";
+
+        // 문자 -> byte
+        byte[] writeByte = writeString.getBytes(UTF_8);
+        System.out.println("write string = " + writeString);
+        System.out.println("write byte = " + Arrays.toString(writeByte));
+
+        // 파일에 쓰기
+        FileOutputStream fos = new FileOutputStream(FILE_NAME);
+        fos.write(writeByte);
+        fos.close();
+        
+        // 파일에서 읽기
+        FileInputStream fis = new FileInputStream(FILE_NAME);
+        byte[] readBytes = fis.readAllBytes();
+        fis.close();
+
+        // byte -> String utf-8 decoding
+        String readString = new String(readBytes, UTF_8);
+        System.out.println("read byte : " + Arrays.toString(readBytes));
+        System.out.println("read String : " + readString);
+
+
+    }
+
+}
