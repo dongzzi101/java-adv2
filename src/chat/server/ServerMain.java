9a1cdf80965256aaa0c1368d896e923c698b2832
dongwook <dongzzi101@gmail.com>
2025-08-16 05:13:57 +0900

    [채팅 프로그램] - v3 : command pattern

--- src/chat/server/ServerMain.java
+++ src/chat/server/ServerMain.java
@@ -11,7 +11,8 @@ public class ServerMain {
 
         // command manager 점진적 변경
 //        CommandManager commandManager = new CommandManagerV1(sessionManager);
-        CommandManager commandManager = new CommandManagerV2(sessionManager);
+//        CommandManager commandManager = new CommandManagerV2(sessionManager);
+        CommandManager commandManager = new CommandManagerV3(sessionManager);
         Server server = new Server(PORT, commandManager, sessionManager);
         server.start();
     }
